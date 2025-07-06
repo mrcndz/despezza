db3078dde48cf53bf677a5b67ddf1c2eb52a9c7a
mrcndz <jrmarcondesb@gmail.com>
2025-07-06 19:54:26 -0300

    fix dev

--- backend/src/api/chat/routes/chat.ts
+++ backend/src/api/chat/routes/chat.ts
@@ -4,29 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default {
-  routes: [
-    // Rotas padrão do Strapi
-    ...factories.createCoreRouter('api::chat.chat').routes,
-    
-    // Rotas customizadas
-    {
-      method: 'POST',
-      path: '/chats/join-group-chat',
-      handler: 'chat.joinGroupChat',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'GET',
-      path: '/chats/my-chats',
-      handler: 'chat.getMyChats',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+export default factories.createCoreRouter('api::chat.chat');
--- backend/src/api/message/routes/message.ts
+++ backend/src/api/message/routes/message.ts
@@ -4,29 +4,4 @@
 
 import { factories } from '@strapi/strapi';
 
-export default {
-  routes: [
-    // Rotas padrão do Strapi
-    ...factories.createCoreRouter('api::message.message').routes,
-    
-    // Rotas customizadas
-    {
-      method: 'POST',
-      path: '/messages/send',
-      handler: 'message.sendMessage',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'PUT',
-      path: '/messages/:id/read',
-      handler: 'message.markAsRead',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-  ],
-};
+export default factories.createCoreRouter('api::message.message');
